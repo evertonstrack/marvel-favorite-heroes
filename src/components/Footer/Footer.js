60340487053fdbb3a2d9144f887cc1767b12b16a
evertonstrack <everton.strack@gmail.com>
2019-03-30 17:12:21 -0300

    Add editor config and fix eol

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './footer.scss';
-
-export const Footer = () => (
-  <footer className="footer" role="contentinfo">
-    <p>made with care by @evertonstrack ♥</p>
-  </footer>
-);
